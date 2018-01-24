67e037ad2ef8473e8e57a8c3ea80360772c8082d
MercsTech01 <33487216+MercsTech01@users.noreply.github.com>
2018-01-24 09:18:08 -0500

    Update Skill.js

--- scripts/Skill.js
+++ scripts/Skill.js
@@ -128,11 +128,17 @@ function Skill(n)
 						damage = target[i].calculateDamage(self, getTypeBonus(self.type, target[i].type));
 						target[i].health.base = Math.max(0, target[i].health.base - damage);	
 						
-						gameLog.write(string.format("PLAYER{0}: {1} does {2} damage to {3}.", 
+						/*gameLog.write(string.format("PLAYER{0}: {1} does {2} damage to {3}.", 
 							(self.player == player1 ? "1" : "2"),
 							self.name, 
 							damage,
 							target[i].name
+						));*/
+						gameLog.write(string.format("PLAYER{0}: {1} {2} {3} DAMAGE.", 
+							(self.player == player1 ? "1" : "2"),
+							self.name, 
+							self.getLastAttack().text,
+							damage
 						));
 					}
 					else
