6401bce7a351b07d10291653defac71f2c55ec97
MercsTech01 <33487216+MercsTech01@users.noreply.github.com>
2018-02-10 15:26:53 -0500

    Update Game.js

--- scripts/Game.js
+++ scripts/Game.js
@@ -54,7 +54,7 @@ var Game =
 		{ background : "mars.jpg", floor : "mars.jpg", sound : "" },
 		{ background : "Spacecity.png", floor : "spacecity.jpg", sound : "" },
 		{ background : "Underwater.png", floor : "underwater.jpg", sound : "" },
-		{ background : "Forest.png", floor : "log.jpg", sound : "" }
+		{ background : "Forest.png", floor : "ForestFloor.jpg", sound : "" }
 	],
 	toggleSound : function()
 	{
