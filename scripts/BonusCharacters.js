6feeddd06533c997c866d3a4e0f74387c74285e9
MercsTech01 <33487216+MercsTech01@users.noreply.github.com>
2018-02-28 09:35:23 -0500

    Update BonusCharacters.js

--- scripts/BonusCharacters.js
+++ scripts/BonusCharacters.js
@@ -30,7 +30,10 @@ function Nemesis()
 	this.accuracy = { modifier : 1.0, duration : -1 };
 
 	this.quote = "";
-    this.backstory = "";
+    this.backstory = "Nemesis is  ■ ■■■■■■■ ■■■ ■■ ■■■■■■■■■■■■■■ ■■■ ■■■■  Cerotis. ■ ■■■ ■■■■■■■■■ ■ ■■■■ ■■■■■ "
+	+ "■■■■■■■■■ ■■■■■■■■■■ ■■■■■. ■■■■■■■■■■■ ■■■■■ ■■■ dead ■■■■■■■■■■■ ■■■■■■■ ■■■ ■■■■■ ■ Killed. ■■■■■■■■ "
+	+ "■■■ ■■ ■■ ■■■■■■■■■■ ■■■■■ ■■■■■■ ■ ■■■■■■■■■■■ ■■■■■■■■■■ ■■■■■■■ ■■■ ■■ hunting ■■■■■■■ ■ ■■■■■■■■■ ■■■■ "
+	+ "■■■■■■ ■■ ■ ■■■■ ■■■ ■■ ■■■■■■■■■  ■■■■■ ■■■■■■■■■.";
 	
 	this.damageImage = "characters/BSGComicStills/Nemesis_Damaged.png";
 	this.defeatImage = "characters/BSGComicStills/Nemesis_Defeat.png";
@@ -63,7 +66,9 @@ function SamuraiGirl()
 	this.accuracy = { modifier : 1.0, duration : -1 };
 
 	this.quote = "";
-    this.backstory = "";
+    this.backstory = "Tatsu applies her samurai arts as a personal bodyguard for hire, working alone or as part of a "
+	+ "small team. On the battlefield, she relies on an overwhelming offence to deter would be attackers, although "
+	+ "she prefers to avoid conflicts when possible.";
 }
 SamuraiGirl.prototype = new Character();
 
@@ -93,7 +98,10 @@ function DinoGirl()
 	this.accuracy = { modifier : 1.0, duration : -1 };
 
 	this.quote = "";
-    this.backstory = "";
+    this.backstory = "Qastra is the living experiment of the mad scientist known as Doctor Jurassic, who made a "
+	+ "life-long career out of his ancient research and brutal cross-DNA mutation procedures. As these things "
+	+ "typically go, Qastra broke out of Jurassic’s laboratory and is struggling with the concept of humanity as "
+	+ "she fights her way to freedom.";
 }
 DinoGirl.prototype = new Character();
 
@@ -124,6 +132,8 @@ function Clown()
 
 
 	this.quote = "";
-    this.backstory = "";
+    this.backstory = "Once a carnival clown, Mr. Giggles has now cast off the laws of his new world, just as he cast off "
+	+ "the chains of mortality. Now he serves the Great Destroyer and plans to bring what remains of the tattered "
+	+ "universe to a dark and dreadful end, in the hope that one day he will be justly rewarded.";
 }
 Clown.prototype = new Character();
