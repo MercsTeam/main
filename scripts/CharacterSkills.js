20d692d9ad43ed9db6bada098b66aeb7e38b2ab0
MercsTech01 <33487216+MercsTech01@users.noreply.github.com>
2018-03-19 15:14:21 -0400

    Update CharacterSkills.js

--- scripts/CharacterSkills.js
+++ scripts/CharacterSkills.js
@@ -1201,6 +1201,7 @@ function TailWhip()
 	this.cooldown = 1;
 	this.accuracy = 0.5;
 	this.description = "A powerful bludgeoning attack with a significant chance of missing.";
+	this.imageURL = "characters/DinoGirlComicStills/TailWhip.png";
 }
 TailWhip.prototype = new Skill("Tail Whip");
 
